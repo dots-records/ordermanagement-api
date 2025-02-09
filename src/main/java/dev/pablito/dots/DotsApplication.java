2522b65c55a1c824cb52331213e9e9c2f788dc2a
pablo <pablo@DESKTOP-4IUS6OH>
2025-02-09 21:24:26 +0100

    test

--- src/main/java/dev/pablito/dots/DotsApplication.java
+++ src/main/java/dev/pablito/dots/DotsApplication.java
@@ -1,20 +1,10 @@
 package dev.pablito.dots;
 
-package dev.pablito.dots;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
